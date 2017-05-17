f5a8e2a16f71d576b0306aaf3153b21abc56c6e6
morgan greenwalt <you@example.com>
2017-05-17 18:51:53 -0500

    css and js edits

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,42 +2,54 @@ $(document).ready(function(){
 
 //Variables 
 var topics = ['the office', 'michael scott', 'pam beesley', 'jim halpert', 'thats what she said'];
-// var newBtn = $("<div>");
 
-for (var i = 0; i < topics.length; i++) {
-	$(".gif-buttons").append("<button type='button' data-name='"+ topics[i] +"' class='btn btn-primary'> "+ topics[i] +" </button>");
+function displayButtons() {
+	for (var i = 0; i < topics.length; i++) {
+		$(".gif-buttons").append("<button type='button' data-name='"+ topics[i] +"' class='btn btn-primary'> "+ topics[i] +" </button>");
+	}
+}
+displayButtons();
+
+function displayGiphy(){
+	$(".btn-primary").on("click", function(){
+	//Variables defining Giphy API URL & user search term	
+		var currentBtn = $(this).attr("data-name");
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q="+currentBtn+"&api_key=dc6zaTOxFJmzC&limit=10";
+
+	//AJAX call to Giphy API	
+		$.ajax ({
+			url: queryURL,
+			method: "GET"
+		}).done(function(results){
+
+	//Variables for Giphy object
+			var gif = results.data;	
+			
+			for (var i = 0; i < gif.length; i++) {
+
+				var rating = gif[i].rating;
+				var still = gif[i].images.fixed_height_still.url;
+				var animate = gif[i].images.fixed_height.url;
+
+				$(".first-three-gifs").prepend("<div class='col-md-4 giftastic'> <img src='"+ still +"'> <p class='rating'>Gif Rating:  "+ rating +" </p> </div>");
+				// $(".giftastic").on("click", function(){
+				// 	$(this).html("<div class='col-md-4 giftastic'> <img src='"+ animate +"'> </div>");
+				// });
+			}
+		})
+
+	});
 }
 
-$(".btn-primary").on("click", function(){
-//Variables defining Giphy API URL & user search term	
-	var currentBtn = $(this).attr("data-name");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+currentBtn+"&api_key=dc6zaTOxFJmzC&limit=10";
-
-//AJAX call to Giphy API	
-	$.ajax ({
-		url: queryURL,
-		method: "GET"
-	}).done(function(results){
-
-//Variables for Giphy object
-		var gif = results.data;	
-		
-		for (var i = 0; i < gif.length; i++) {
-
-			var rating = gif[i].rating;
-			var still = gif[i].images.fixed_height_still.url;
-			var animate = gif[i].images.fixed_height.url;
-
-			$(".first-three-gifs").append("<div class='giftastic'> <p class='rating'>Rating:  "+ rating +" </p> <img src='"+ still +"'> </div>");
-		}
-	})
-
+//On click function to store user search term and run ajax call
+$("#searchBtn").on("click", function(){
+	event.preventDefault();
+	var newTopic = $("#srch-term").val().trim();
+	topics.push(newTopic);
+	$(".gif-buttons").append("<button type='button' data-name='"+ newTopic +"' class='btn btn-primary'> "+ newTopic +" </button>");
 });
 
-//On click function to store user search term and run ajax call
-// $("#searchBtn").on("click", function(){
-
-// });
+$(document).on("click", ".gif-buttons", displayGiphy);
 
 //Animate gifs on click
 // $(".giftastic").on("click", function(){
