e5958b1beeee7c68c397b87c0a6082acde374137
morgan greenwalt <you@example.com>
2017-05-16 20:43:13 -0500

    clicks

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -5,32 +5,31 @@ var topics = ['the office', 'michael scott', 'pam beesley', 'jim halpert', 'that
 // var newBtn = $("<div>");
 
 for (var i = 0; i < topics.length; i++) {
-	$(".gif-buttons").append("<button type='button' name=' "+ topics[i] +"' class='btn btn-primary'> "+ topics[i] +" </button>");
+	$(".gif-buttons").append("<button type='button' data-name='"+ topics[i] +"' class='btn btn-primary'> "+ topics[i] +" </button>");
 }
 
 $(".btn-primary").on("click", function(){
 //Variables defining Giphy API URL & user search term	
-	var currentBtn = $(this).attr("name");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ currentBtn +"&api_key=dc6zaTOxFJmzC&limit=10";
-	// var userSearch = $("#srch-term").val();
+	var currentBtn = $(this).attr("data-name");
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+currentBtn+"&api_key=dc6zaTOxFJmzC&limit=10";
 
 //AJAX call to Giphy API	
 	$.ajax ({
 		url: queryURL,
-		method: 'GET'
+		method: "GET"
 	}).done(function(results){
 
 //Variables for Giphy object
-		var gif = results.data[0];	
-		var rating = gif.rating;
-		var still = gif.images.fixed_height_still.url;
-		var animate = gif.images.fixed_height.url;
-		console.log(rating);	
-		console.log(gif);
-		console.log(still);
-		console.log(animate);
+		var gif = results.data;	
+		
+		for (var i = 0; i < gif.length; i++) {
 
-		$(".first-three-gifs").append("<div class='giftastic'> <p class='rating'> "+ rating +" </p> <img src='"+ animate +"'> </div>");
+			var rating = gif[i].rating;
+			var still = gif[i].images.fixed_height_still.url;
+			var animate = gif[i].images.fixed_height.url;
+
+			$(".first-three-gifs").append("<div class='giftastic'> <p class='rating'>Rating:  "+ rating +" </p> <img src='"+ still +"'> </div>");
+		}
 	})
 
 });
@@ -38,6 +37,12 @@ $(".btn-primary").on("click", function(){
 //On click function to store user search term and run ajax call
 // $("#searchBtn").on("click", function(){
 
+// });
+
+//Animate gifs on click
+// $(".giftastic").on("click", function(){
+// 	// var animate = gif.images.fixed_height.url;
+// 	$(".first-three-gifs").html("<img src='"+ animate +"'>");
 // });
 
 });
